bb83292554b7448632303ef19f1fba364f0e074a
Wellington Ramos Bezerra <wellingtons.bezerra@hotmail.com>
2025-05-06 21:11:31 -0300

    Iniciando projeto

---
+++ src/main/java/com/fakeitau/desafioconta/DesafiocontaApplication.java
@@ -0,0 +1,13 @@
+package com.fakeitau.desafioconta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiocontaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiocontaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fakeitau/desafioconta/DesafiocontaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakeitau.desafioconta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiocontaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
