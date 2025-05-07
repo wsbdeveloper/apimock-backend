935cc7f1aa0905933498a6e8f9762f1938088ed3
Wellington Ramos Bezerra <wellingtons.bezerra@hotmail.com>
2025-05-06 21:36:36 -0300

    feat: adicionando camada controller e teste de retorno

---
+++ src/main/java/com/fakeitau/desafioconta/application/controller/ContaController.java
@@ -0,0 +1,25 @@
+package com.fakeitau.desafioconta.application.controller;
+
+
+import com.fakeitau.desafioconta.service.ContaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/conta")
+public class ContaController {
+
+    private final ContaService contaService;
+
+    public ContaController(ContaService contaService){
+        this.contaService = contaService;
+    }
+
+    @GetMapping("/{idConta}")
+    public ResponseEntity buscarDadosConta(@PathVariable String idConta){
+        return ResponseEntity.ok(contaService.consultarConta("123"));
+    }
+}
